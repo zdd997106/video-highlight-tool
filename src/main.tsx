c336b23b993328d19541c1c97357ea8d5390d33b
Zdd Zhong <zz997106@gmail.com>
2025-04-18 17:29:12 +0800

    chore: Adjust import order

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { ThemeProvider } from "@mui/material";
+import { DialogsProvider } from "gexii/dialogs";
 
 import "./global.css";
 import Pages from "./pages";
 import { theme } from "./theme";
-import { DialogsProvider } from "gexii/dialogs";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
